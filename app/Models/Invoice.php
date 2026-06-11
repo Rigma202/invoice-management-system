82bc59d0f911463f8901351b0b53e0bc433fdd3d
rigmaumesh@gmail.com <rigmaumesh34@gmail.com>
2026-06-11 17:22:36 +0530

    Added migrations, model and controller structure

---
+++ app/Models/Invoice.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Invoice extends Model
+{
+ use  SoftDeletes;
+
+    protected $fillable = [
+        'customer_id', 'product_id', 'invoice_date',
+        'due_date', 'quantity', 'unit_price', 'total_amount'
+    ];
+
+    protected $casts = [
+        'invoice_date' => 'date',
+        'due_date'     => 'date',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
